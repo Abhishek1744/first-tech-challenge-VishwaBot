61cfc6fa2088600dde1eeb362c9c2350661f551b
VishwaBot - Team 01 <team.animators1024@gmail.com>
2026-01-23 21:28:24 +0530

    autonomousCodeByTeam01

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicAutonomous.java
@@ -9,6 +9,9 @@ public class BasicAutonomous extends LinearOpMode {
 
     DcMotor leftFront, leftBack, rightFront, rightBack;
 
+    // Tune this value on-field
+    private static final int TURN_45_TIME_MS = 450;
+
     @Override
     public void runOpMode() {
 
@@ -29,74 +32,82 @@ public class BasicAutonomous extends LinearOpMode {
 
         if (opModeIsActive()) {
 
-            // Move forward
-            setPower(0.5);
+            // Forward
+            driveForward(0.5);
             sleep(2000);
+            stopDrive();
 
-            // Stop
-            setPower(0);
+            sleep(300);
+
+            // Strafe right
+            strafeRight(0.8);
+            sleep(1500);
+            stopDrive();
+
+            sleep(300);
+
+            // 🔄 45-degree right turn
+            turnRight(0.5);
+            sleep(TURN_45_TIME_MS);
+            stopDrive();
+
+            sleep(300);
+
+            // Backward
+            driveBackward(1.0);
+            sleep(200);
+            stopDrive();
+
+            sleep(300);
+
+            // Strafe left
+            strafeLeft(0.5);
+            sleep(3000);
+            stopDrive();
         }
     }
 
-    private void setPower(double power) {
+    /* ---------- Drive Methods ---------- */
+
+    private void driveForward(double power) {
         leftFront.setPower(power);
         leftBack.setPower(power);
         rightFront.setPower(power);
         rightBack.setPower(power);
     }
+
+    private void driveBackward(double power) {
+        leftFront.setPower(-power);
+        leftBack.setPower(-power);
+        rightFront.setPower(-power);
+        rightBack.setPower(-power);
+    }
+
+    private void strafeRight(double power) {
+        leftFront.setPower(power);
+        leftBack.setPower(-power);
+        rightFront.setPower(-power);
+        rightBack.setPower(power);
+    }
+
+    private void strafeLeft(double power) {
+        leftFront.setPower(-power);
+        leftBack.setPower(power);
+        rightFront.setPower(power);
+        rightBack.setPower(-power);
+    }
+
+    private void turnRight(double power) {
+        leftFront.setPower(power);
+        leftBack.setPower(power);
+        rightFront.setPower(-power);
+        rightBack.setPower(-power);
+    }
+
+    private void stopDrive() {
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+    }
 }
-
-
-//package org.firstinspires.ftc.teamcode.autonomous;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//
-//@Autonomous(name = "Basic Autonomous", group = "Autonomous")
-//public class BasicAutonomous extends LinearOpMode {
-//
-//    DcMotor leftFront, leftBack, rightFront, rightBack;
-//
-//    @Override
-//    public void runOpMode() {
-//
-//        // Hardware mapping
-//        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
-//        leftBack   = hardwareMap.get(DcMotor.class, "leftBack");
-//        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-//        rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
-//
-//        // Reverse one side (depends on your build)
-//        rightFront.setDirection(DcMotor.Direction.REVERSE);
-//        rightBack.setDirection(DcMotor.Direction.REVERSE);
-//
-//        // Brake when power = 0
-//        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//
-//        telemetry.addLine("Autonomous Ready");
-//        telemetry.update();
-//
-//        waitForStart();
-//
-//        if (opModeIsActive()) {
-//
-//            // Move forward
-//            setPower(0.5);
-//            sleep(2000);
-//
-//            // Stop
-//            setPower(0);
-//        }
-//    }
-//
-//    private void setPower(double power) {
-//        leftFront.setPower(power);
-//        leftBack.setPower(power);
-//        rightFront.setPower(power);
-//        rightBack.setPower(power);
-//    }
-//}
